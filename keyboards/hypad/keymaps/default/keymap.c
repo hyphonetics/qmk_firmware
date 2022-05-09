7a407cf56ac540e38f7786157b24a58091b6b869
hyphonetics <67212226+hyphonetics@users.noreply.github.com>
2022-05-09 11:53:31 -0700

    another try

--- keyboards/hypad/keymaps/default/keymap.c
+++ keyboards/hypad/keymaps/default/keymap.c
@@ -7,15 +7,15 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┐
-     * │TG1│ /    │ *    │ - │
+     * │TG1│ / │ * │ - │
      * ├───┼───┼───┼───┤
-     * │ 7   │ 8    │ 9    │ + │
+     * │ 7 │ 8 │ 9 │ + │
      * ├───┼───┼───┼───┤
-     * │ 4    │ 5   │ 6    │ %   │
+     * │ 4 │ 5 │ 6 │ % │
      * ├───┼───┼───┼───┤
-     * │ 1    │ 2   │ 3    │ =   │
+     * │ 1 │ 2 │ 3 │ = │
      * ├───┼───┼───┼───┤
-     * │ 0    │00  │ .    │Ent│
+     * │ 0 │00 │ . │Ent│
      * └───┴───┴───┴───┘
      */
     [0] = LAYOUT_ortho_5x4(
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
         KC_P4,   KC_P5,   KC_P6,   KC_PERC,
         KC_P1,   KC_P2,   KC_P3,   KC_EQL,
-        TG(1),   KC_P0,  KC_PDOT, KC_PENT
+        KC_P0,   KC_P00,  KC_PDOT, KC_PENT
     ),
 
     /*
